43093808b60e55c2e38d4ab8d7796f6d0318c2df
Yash <vyash7265@gmail.com>
2024-09-09 11:52:21 +0530

    Home page updated

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
 
 const Layout = ({children}:{children:React.ReactNode}) => {
   return (
     <main className='root'>
-        <Header />
         {children}
-        <Footer />
+      
     </main>
 )
 }
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -4,13 +4,17 @@ import React from "react";
 import Section1 from "../components/Section1";
 import Section2 from "../components/Section2";
 import Section3 from "../components/Section3";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Home: React.FC = () => {
   return (
     <main className="flex-grow min-h-screen min-w-screen bg-[url('/sec2_bg3.jpg')] bg-cover bg-center bg-fixed bg-no-repeat">
+      <Header />
       <Section1 />
       <Section2 />
       <Section3 />
+      <Footer />
     </main>
   );
 };
